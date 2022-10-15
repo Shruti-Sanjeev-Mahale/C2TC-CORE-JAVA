0dcae190ffdd218027c801b57fd6eaa9bca88c95
Shruti-Sanjeev-Mahale <115076748+Shruti-Sanjeev-Mahale@users.noreply.github.com>
2022-10-15 20:43:16 +0530

    Update FirstProgram.java

--- src/com/cg/corejava/FirstProgram.java
+++ src/com/cg/corejava/FirstProgram.java
@@ -4,7 +4,7 @@ public class FirstProgram {
 
 	public static void main(String[] args) 
 	{
-	     System.out.println("Welcome M26 Batch !!!!");
+	     System.out.println("Welcome to my M26 Batch !!!!");
 
 	}
 
